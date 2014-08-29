48a3da256233e917bd7e1bcd0cc4bd2826fffe08
Marco Lechner, in medias res GmbH <lechner@webgis.de>
2014-08-29 11:05:42 +0200

    added post-file flag to support old wget

--- switch_edimax.sh
+++ switch_edimax.sh
@@ -9,21 +9,25 @@ ON_XML=./on.xml
 OFF_XML=./off.xml
 STATUS_XML=./status.xml
 HEADER='Content-Type:text/xml'
+PF=0
 
 URL=$PROTO'://'$USER':'$PASS'@'$IP':'$PORT
 
 Usage () {
   echo "Usage:"
-  echo "./switch_edimax.sh [-10su]"
+  echo "./switch_edimax.sh [-pf][-10su]"
   echo ""
   echo "Switch Edimax on, off or request status"
   echo ""
+  echo "Flag:"
+  echo "    -pf or --post-file  Send xml via wgets post-file - not available at older wget"
+  echo ""
   echo "Options:"
-  echo "    -1 or --on        Send on.xml to Edimax Smartplug"
-  echo "    -0 or --off       Send off.xml to Edimax Smartplug"
-  echo "    -s or --status    Send status.xml to Edimax Smartplug"
-  echo "    -h or --help      Print this Usage"
-  echo "    -u or --usage      Print this Usage"
+  echo "    -1  or --on         Send on.xml to Edimax Smartplug"
+  echo "    -0  or --off        Send off.xml to Edimax Smartplug"
+  echo "    -s  or --status     Send status.xml to Edimax Smartplug"
+  echo "    -h  or --help       Print this Usage"
+  echo "    -u  or --usage      Print this Usage"
   echo ""
 }
 
@@ -34,20 +38,52 @@ if [ $# -lt 1 ]
     exit 1
 fi
 
+while [ "$#" -gt "0" ] && [ "$PF" -eq "0" ]
+do
+  case $1 in
+    -pf|--post-file)
+      shift
+      PF=1
+      break
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 while [ "$#" -gt "0" ]
 do
   case $1 in
     -1|--on)
       shift
-      wget $URL --post-file=$ON_XML --header=$HEADER
+      if [ "$PF" -eq "1" ]
+        then
+          wget $URL --post-data="$(cat $ON_XML)" --header=$HEADER
+        else
+          wget $URL --post-file=$ON_XML --header=$HEADER
+      fi
+      exit 0
       ;;
     -0|--off)
       shift
-      wget $URL --post-file=$OFF_XML --header=$HEADER
+      if [ "$PF" -eq "1" ]
+        then
+          wget $URL --post-data="$(cat $OFF_XML)" --header=$HEADER
+        else
+          wget $URL --post-file=$OFF_XML --header=$HEADER
+      fi
+      exit 0
       ;;
     -s|--status)
       shift
-      wget $URL --post-file=$STATUS_XML --header=$HEADER
+      if [ "$PF" -eq "1" ]
+        then
+          wget $URL --post-data="$(cat $STATUS_XML)" --header=$HEADER
+        else
+          wget $URL --post-file=$STATUS_XML --header=$HEADER
+      fi
+      exit 0
       ;;
     -u|-h|--help|--usage)
       shift
